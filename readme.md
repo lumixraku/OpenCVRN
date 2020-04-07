ab66bcad82dd8bb24d5060bb708bca963ae0d3ee
Nan Luo <nan.luo@shopee.com>
2020-04-07 13:24:41 +0800

    add camera scene

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+## Trouble shooting
+
+Cannot choose between the following variants of project :react-native-camera
+
+https://github.com/react-native-community/react-native-camera/issues/2138
--- src/Screens/CameraScreen.js
+++ src/Screens/CameraScreen.js
@@ -1,112 +1,105 @@
+import React, {Component} from 'react';
 import {
-  StyleSheet,
+  AppRegistry,
+  View,
+  Text,
+  Platform,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
+import {RNCamera as Camera} from 'react-native-camera';
+import Toast, {DURATION} from 'react-native-easy-toast';
 
-export default StyleSheet.create({
-  imagePreview: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    left: 0,
-    bottom: 60,
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  repeatPhotoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    width: '50%',
-    height: 120,
-    backgroundColor: '#000',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
-  topButtonsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  focusFrameContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    height: '100%',
-    width: '100%',
-  },
-  focusFrame: {
-    height: 90,
-    width: 90,
-    borderWidth: 1,
-    borderColor: '#fff',
-    borderStyle: 'dotted',
-    borderRadius: 5,
-  },
-  photoPreviewRepeatPhotoText: {
-    color: '#abcfff',
-    fontSize: 15,
-    marginLeft: 10,
-  },
-  usePhotoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    width: '50%',
-    height: 120,
-    backgroundColor: '#000',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-  },
-  photoPreviewUsePhotoText: {
-    color: '#abcfff',
-    fontSize: 15,
-    marginRight: 10,
-  },
-  preview: {
-    position: 'relative',
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  takePictureContainer: {
-    position: 'absolute',
-    paddingVertical: 20,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-o is blurred!',DURATION.FOREVER);
-        return this.repeatPhoto();
+import styles from '../Styles/Screens/CameraScreen';
+import OpenCV from '../NativeModules/OpenCV';
+import CircleWithinCircle from '../assets/svg/CircleWithinCircle';
+
+export default class CameraScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.takePicture = this.takePicture.bind(this);
+    this.checkForBlurryImage = this.checkForBlurryImage.bind(this);
+    this.proceedWithCheckingBlurryImage = this.proceedWithCheckingBlurryImage.bind(
+      this,
+    );
+    this.repeatPhoto = this.repeatPhoto.bind(this);
+    this.usePhoto = this.usePhoto.bind(this);
+  }
+
+  state = {
+    cameraPermission: false,
+    photoAsBase64: {
+      content: '',
+      isPhotoPreview: false,
+      photoPath: '',
+    },
+  };
+
+  checkForBlurryImage(imageAsBase64) {
+    return new Promise((resolve, reject) => {
+      if (Platform.OS === 'android') {
+        OpenCV.checkForBlurryImage(
+          imageAsBase64,
+          (error) => {
+            // error handling
+          },
+          (msg) => {
+            resolve(msg);
+          },
+        );
+      } else {
+        OpenCV.checkForBlurryImage(imageAsBase64, (error, dataArray) => {
+          resolve(dataArray[0]);
+        });
       }
-      this.refs.toast.show('Photo is clear!', DURATION.FOREVER);
-      this.setState({ photoAsBase64: { ...this.state.photoAsBase64, isPhotoPreview: true, photoPath } });
-    }).catch(err => {
-      console.log('err', err)
     });
+  }
+
+  proceedWithCheckingBlurryImage() {
+    const {content, photoPath} = this.state.photoAsBase64;
+
+    this.checkForBlurryImage(content)
+      .then((blurryPhoto) => {
+        if (blurryPhoto) {
+
+          console.log("blur photo");
+
+          this.refs.toast.show('Photo is blurred!', DURATION.FOREVER);
+          return this.repeatPhoto();
+        }
+
+        console.log('clear photo');
+
+        this.refs.toast.show('Photo is clear!', DURATION.FOREVER);
+        this.setState({
+          photoAsBase64: {
+            ...this.state.photoAsBase64,
+            isPhotoPreview: true,
+            photoPath,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
   }
 
   async takePicture() {
     if (this.camera) {
-      const options = { quality: 0.5, base64: true };
+      const options = {quality: 0.5, base64: true};
       const data = await this.camera.takePictureAsync(options);
       this.setState({
         ...this.state,
-        photoAsBase64: { content: data.base64, isPhotoPreview: false, photoPath: data.uri },
+        photoAsBase64: {
+          content: data.base64,
+          isPhotoPreview: false,
+          photoPath: data.uri,
+        },
       });
       this.proceedWithCheckingBlurryImage();
     }
   }
-
 
   repeatPhoto() {
     this.setState({
@@ -123,14 +116,15 @@ o is blurred!',DURATION.FOREVER);
     // do something, e.g. navigate
   }
 
-
   render() {
     if (this.state.photoAsBase64.isPhotoPreview) {
       return (
         <View style={styles.container}>
           <Toast ref="toast" position="center" />
           <Image
-            source={{ uri: `data:image/png;base64,${this.state.photoAsBase64.content}` }}
+            source={{
+              uri: `data:image/png;base64,${this.state.photoAsBase64.content}`,
+            }}
             style={styles.imagePreview}
           />
           <View style={styles.repeatPhotoContainer}>
@@ -142,9 +136,7 @@ o is blurred!',DURATION.FOREVER);
           </View>
           <View style={styles.usePhotoContainer}>
             <TouchableOpacity onPress={this.usePhoto}>
-              <Text style={styles.photoPreviewUsePhotoText}>
-                Use photo
-              </Text>
+              <Text style={styles.photoPreviewUsePhotoText}>Use photo</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -154,13 +146,14 @@ o is blurred!',DURATION.FOREVER);
     return (
       <View style={styles.container}>
         <Camera
-          ref={cam => {
+          ref={(cam) => {
             this.camera = cam;
           }}
           style={styles.preview}
           permissionDialogTitle={'Permission to use camera'}
-          permissionDialogMessage={'We need your permission to use your camera phone'}
-        >
+          permissionDialogMessage={
+            'We need your permission to use your camera phone'
+          }>
           <View style={styles.takePictureContainer}>
             <TouchableOpacity onPress={this.takePicture}>
               <View>
@@ -174,6 +167,5 @@ o is blurred!',DURATION.FOREVER);
     );
   }
 }
-
 
 AppRegistry.registerComponent('CameraScreen', () => CameraScreen);
