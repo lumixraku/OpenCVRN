4e679e2809ce41ff102449d39118607cbd3237c8
raku <lumixraku@gmail.com>
2020-04-08 18:57:38 +0800

    get pos from react

--- src/util/util.js
+++ src/util/util.js
@@ -1,7 +1,28 @@
+
+import _ from "underscore";
+
 const	postToWebview = (webview, data) => {
+	  // Ok postMessage 可以放对象
+		// var runJS = `
+		// // 	window.postMessage({a: 1})
+		// `;
+		// 当 data = {a:1} 时
+		// var runJS = `
+		// 	window.postMessage(${data})
+		// `;
+		// 这么做实际上是 window.postMessage([object Object])
+
+		var dataStr = ""
+		if ( typeof(data) != "string" ) {
+			dataStr = JSON.parse(data)
+		}else{
+			dataStr = data;
+		}
+		// dataStr = dataStr.replace('"', '\"')
 		var runJS = `
-			window.postMessage("...........")
+			window.postMessage(${dataStr})
 		`;
+
 		console.log("LogDemo :::: runJS ", runJS);
 		webview.injectJavaScript(runJS);
   }
--- src/webpage/index.js
+++ src/webpage/index.js
@@ -1,3 +1,15 @@
+var c = document.getElementById('canvas');
+var ctx = c.getContext('2d');
+c.width = document.body.clientWidth
+c.height = document.body.clientHeight;
+// Red rectangle
+// ctx.beginPath();
+// ctx.lineWidth = "6";
+// ctx.strokeStyle = "red";
+// ctx.rect(2, 54, 192, 147);
+// ctx.stroke();
+
+
 window.addEventListener("message", receivePostMessage, false);
 var infoDiv = document.querySelector("#info")
 function receivePostMessage(event) {
@@ -6,11 +18,27 @@ function receivePostMessage(event) {
 	// 这里不准确，chrome没有这个属性
 	// var origin = event.origin || event.originalEvent.origin;
 	var origin = event.origin;
+	console.log("screen size:::", c.clientWidth, c.clientHeight)
 	console.log("event FROM RN::::", typeof(event.data),  event.data)
 
+	drawRect(event.data)
 
   if (window.ReactNativeWebView) {
 		window.ReactNativeWebView.postMessage("Hello! From JS");
 	}
+
+
 	// ...
+}
+
+function drawRect(data) {
+	if (data.facepos && data.facepos.length == 4) {
+
+		ctx.beginPath();
+		ctx.lineWidth = '6';
+		ctx.strokeStyle = 'red';
+		ctx.rect(2, 54, 292, 347);
+		ctx.stroke();
+	}
+
 }
